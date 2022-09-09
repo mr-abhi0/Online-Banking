5a82247d92e60a70a7e5a121d37edd1dfa4dcf0b
mr-abhi0 <durgapr09138@gmail.com>
2022-09-09 13:22:43 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+<div class="header_main">
+	<div class="navBar">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="index.php#aboutus">About Us</a></li>
+			<li><a href="index.php#contactus">Contact Us</a></li>
+			<li><a href="staff_login.php">Staff Login</a></li>
+
+		</ul>
+	</div>
+	<a href="index.php">
+		<div class="logo-name">
+			<div class="logo">
+				<img class="logo_img" src="img/download.jpg" height="90" width="100">
+			</div>
+
+			<div class="name">
+				<h5>Swift International Bank</h5>
+	</a><br>
+
+</div>
+</div>
+
+
+<div class="dif_banking">
+	<div class="retail_banking">
+		<a href="customer_reg_form.php">New Account</a>
+	</div>
+	<div class="corporate_banking">
+		<a href="debit_card_form.php"> Apply Debit Card</a>
+	</div>
+	<div class="international_banking">
+		<a href="ebanking_reg_form.php">Netbanking</a>
+	</div>
+	<div class="international_banking">
+		<a href="customer_login.php"> Your Account</a>
+	</div>
+
+	<div class="bank_servic">
+		<a href="staff_login.php">Administrator</a>
+	</div>
+</div>
+
+
+
+</div>
